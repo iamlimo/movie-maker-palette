43b3cdd3d90ecb69b902eafe911ddb96d3f3934b
iamlimo <isaac.orija@gmail.com>
2026-04-17 12:40:09 +0100

    vercel cache

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 // Simple render with basic error handling
 const rootElement = document.getElementById('root');
 if (rootElement) {
